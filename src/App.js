084d5e29560986365c51bdf621a3c75f3a50d54d
PaulZChristy2017 <paul.christy2017@gmail.com>
2019-07-28 20:54:49 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import './App.css';
+
+import MaterialTable from 'material-table';
+import {} from 'material-icons';
+
+export default function App() {
+    const [state, setState] = React.useState({
+        columns: [
+            { title: 'Event #', field: 'eventNumber', type: 'numeric' },
+            { title: 'Customer', field: 'customer' },
+            { title: 'Event', field: 'eventType' },
+            { title: 'Date', field: 'date' },
+            { title: 'Time', field: 'time', },
+        ],
+        data: [{
+                eventNumber: '1235',
+                customer: 'Mike Bickers',
+                eventType: 'Meeting',
+                date: '05/05/20',
+                time: '4:00',
+            },
+
+            {
+                eventNumber: '1236',
+                customer: 'David Slaughter',
+                eventType: 'Meeting',
+                date: '05/19/20',
+                time: '1:00',
+            },
+
+            {
+                eventNumber: '1237',
+                customer: 'Mark Payne',
+                eventType: 'Wedding',
+                date: '05/18/20',
+                time: '2:00',
+            },
+
+            {
+                eventNumber: '1239',
+                customer: 'Bob Dewey',
+                eventType: 'Wedding',
+                date: '05/05/20',
+                time: '5:30',
+            },
+        ],
+    });
+
+    return ( <
+        MaterialTable title = "Events"
+        columns = { state.columns }
+        data = { state.data }
+        editable = {
+            {
+                onRowAdd: newData =>
+                    new Promise(resolve => {
+                        setTimeout(() => {
+                            resolve();
+                            const data = [...state.data];
+                            data.push(newData);
+                            setState({...state, data });
+                        }, 600);
+                    }),
+                onRowUpdate: (newData, oldData) =>
+                    new Promise(resolve => {
+                        setTimeout(() => {
+                            resolve();
+                            const data = [...state.data];
+                            data[data.indexOf(oldData)] = newData;
+                            setState({...state, data });
+                        }, 600);
+                    }),
+                onRowDelete: oldData =>
+                    new Promise(resolve => {
+                        setTimeout(() => {
+                            resolve();
+                            const data = [...state.data];
+                            data.splice(data.indexOf(oldData), 1);
+                            setState({...state, data });
+                        }, 600);
+                    }),
+            }
+        }
+        />
+    );
+}
